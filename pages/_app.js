5efda55d1566df75c229f85964102ec355253199
BeedinM <douglas.bedin@gmail.com>
2023-07-18 20:16:32 -0300

    add _app.js

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css';
+
+import Head from 'next/head';
+
+export default function App ( {Component, pageProps} ) {
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                
+                <title>Infinity Monkey</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+};
