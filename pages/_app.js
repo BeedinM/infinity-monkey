bf7f7a46a6b501c9a8bd8c7e60b26a0c1d5790a9
BeedinM <douglas.bedin@gmail.com>
2023-07-25 13:26:33 -0300

    add login e sessão

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 
+import { SessionProvider } from 'next-auth/react';
+
 import Head from 'next/head';
 import Footer from '@/components/footer';
 
@@ -9,16 +11,18 @@ const exo2 = Exo_2({ subsets: ['latin'] })
 
 export default function App ( {Component, pageProps} ) {
     return (
-        <main className={exo2.className}>
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                
-                <title>Infinity Monkey</title>
-            </Head>
-            <Component {...pageProps} />
-            <Footer />
-        </main>
+        <SessionProvider session={pageProps.session}>
+            <main className={exo2.className}>
+                <Head>
+                    <meta charSet="UTF-8" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    
+                    <title>Infinity Monkey</title>
+                </Head>
+                <Component {...pageProps} />
+                <Footer />
+            </main>
+        </SessionProvider>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,25 @@
-import { useEffect, useState } from 'react';
-import SelectModal from '@/components/selectModal';
+import { useRouter } from 'next/router';
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { useEffect } from 'react';
 
-import styles from '../styles/styles.module.css';
-
-const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
-
-async function checkWordInDictionary(word) {
-  const response = await fetch(`https://api.dicionario-aberto.net/word/${word}`);
-  const data = await response.json();
-
-  // Verificar se o objeto de resposta contém dados (propriedades)
-  return Object.keys(data).length > 0;
-}
 
 export default function Inicio() {
-  const [textoMacaco, setTextoMacaco] = useState([]);
-  const [selectedText, setSelectedText] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [palavras, setPalavras] = useState([]);
+    const router = useRouter();
+    /* const isActive = (pathname) => {
+        return router.pathname === pathname;
+    }; */
+    const { data: session} = useSession();
 
-  const handleSelection = () => {
-    const text = window.getSelection().toString();
-    setSelectedText(text);
-  };
-
-  const handleConfirm = async () => {
-    const wordExists = await checkWordInDictionary(selectedText);
-    if (wordExists) {
-      setPalavras((prevPalavras) => [...prevPalavras, selectedText]);
-    } else {
-      alert('Palavra não encontrada no dicionário.');
-    }
-    setShowModal(false);
-  };
-
-  const handleCancel = () => {
-    setShowModal(false);
-  };
-
-  useEffect(() => {
-    // Mostrar o modal somente se houver texto selecionado
-    if (selectedText) {
-      setShowModal(true);
-    } else {
-      setShowModal(false);
-    }
-  }, [selectedText]);
-
-  //script pegar e armazenar uma letra aleatória a x tempo
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTextoMacaco((prevTextoMacaco) => [
-        ...prevTextoMacaco,
-        letras[Math.floor(Math.random() * letras.length)],
-      ]);
-    }, 250);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className={styles.container}>
-      {/* Renderizar o componente de modal somente quando houver texto selecionado */}
-      {showModal && (
-        <SelectModal
-          selectedText={selectedText}
-          onClose={handleCancel}
-          onConfirm={handleConfirm}
-        />
-      )}
-      <div className={styles.tituloIM}>Infinity Monkey</div>
-      <div className={styles.divGif}>
-        <img src='/images/monkey-typing.gif'></img>
-      </div>
-      <div className={styles.divTxt} onMouseUp={handleSelection}>
-        {textoMacaco.map((letra, index) => (
-          <span key={index}>{letra}</span>
-        ))}
-      </div>
-      <div>
-        {palavras.map((palavra, index) => (
-          <p key={index}>{index} {palavra}</p>
-        ))}
-      </div>
-    </div>
-  );
-}
+    useEffect(() => {
+        if (session) {
+            router.push('/jogo');
+        }
+    }, [session, router]);
+    
+    return (
+        <div>
+            <Link href='/api/auth/signin'>FAZER LOGIN</Link>
+        </div>
+    );
+};
