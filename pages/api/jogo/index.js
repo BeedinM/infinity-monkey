da226d9bbea9459deb4eac94e8cf09c944caf744
BeedinM <douglas.bedin@gmail.com>
2023-07-27 13:33:50 -0300

    teste

--- pages/api/jogo/index.js
+++ pages/api/jogo/index.js
@@ -1,20 +1,10 @@
 import prisma from '@/lib/prisma';
 
-async function checkWordInDictionary(word) {
-  const response = await fetch(`https://api.dicionario-aberto.net/word/${word}`);
-  const data = await response.json();
-
-  // Verificar se o objeto de resposta contém dados (propriedades)
-  return Object.keys(data).length > 0;
-}
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { userId, selectedText } = req.body;
 
     try {
-      const wordExists = await checkWordInDictionary(selectedText);
-      if (wordExists) {
         const foundWord = await prisma.foundWord.findFirst({
           where: {
             userId,
@@ -36,10 +26,8 @@ export default async function handler(req, res) {
         });
 
         return res.json({ message: 'Palavra salva com sucesso!', word: newWord });
-      } else {
-        return res.status(400).json({ error: 'Palavra não encontrada no dicionário.' });
-      }
-    } catch (error) {
+      } 
+    catch (error) {
       console.error('Erro ao salvar a palavra:', error);
       return res.status(500).json({ error: 'Erro ao salvar a palavra.' });
     }
